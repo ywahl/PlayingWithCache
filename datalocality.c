585fb5fb0882ae9ff367ff4905cd36ec13783af7
Aron Wahl <awahl@yahoo.com>
2016-01-13 23:26:14 +0200

    Create datalocality.c

---
+++ datalocality.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define size (1 << 10)
+int arr[size][size];
+
+
+int main(int argc, char *argv)
+{
+  int i, j;
+  int closerow = 0;
+  if (argc > 1)
+    closerow = 1;
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      if (closerow)
+	arr[i][j] = 5;
+      else
+	arr[j][i] = 5;
+    }
+  }
+  return 0;
+}
