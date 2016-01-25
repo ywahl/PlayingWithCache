40925318e3050113297f664b467ae0136d367786
awahl <awahl@yahoo.com>
2016-01-25 22:50:30 +0200

    Removed if

--- datalocality.c
+++ datalocality.c
@@ -4,19 +4,33 @@
 int arr[size][size];
 
 
-int main(int argc, char *argv)
+
+void loop_local()
 {
   int i, j;
-  int closerow = 0;
-  if (argc > 1)
-    closerow = 1;
   for (i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
-      if (closerow)
-	arr[i][j] = 5;
-      else
-	arr[j][i] = 5;
-    }
-  }
+    for (j = 0; j < size; j++)
+      arr[i][j] = 5;
+  }  
+}
+
+
+void loop_remote()
+{
+  int i, j;
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++)
+      arr[i][j] = 5;
+  }  
+}
+
+
+
+int main(int argc, char *argv)
+{
+  if (argc > 1)
+    loop_local();
+  else
+    loop_remote();
   return 0;
 }
